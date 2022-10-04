85f73beb2ceff20bf7adf981ce4082b36b592763
Boxuan  Chen <boxuanchen@rafael.78b505.nycboe.org>
2022-10-04 13:03:45 -0400

    favorite-movie

---
+++ 01-favorite-movie/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class movie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+class Main {
+  public static void main(String[] args) {
+    System.out.println("My favorite movie is Insidious");
+    System.out.println("This movie is a scared movie");
+    System.out.println("I really like watching scared movies");
+  }
+}
+  
