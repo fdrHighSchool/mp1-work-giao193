1bdd02da57a0cebbf38d70fcf3d95f6ac8e09f30
Boxuan  Chen <boxuanchen@rafael.78b505.nycboe.org>
2022-10-04 14:15:26 -0400

    rocket

---
+++ 02-rocketship/Rocket.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class Rocket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+class Main {
+  public static void main(String[] args) {
+    roof();
+    base();
+    base2();
+    base3();
+    enging();
+  }
+  public static void roof(){
+    System.out.println("    /\\ ");
+    System.out.println("   /  \\ ");
+    System.out.println("  /    \\ ");
+    System.out.println(" +------+");
+  }
+
+  public static void base(){
+    System.out.println(" |      |");
+    System.out.println(" |      |");
+    System.out.println(" +------+");
+  }
+
+  public static void base2(){
+    System.out.println(" |United|");
+    System.out.println(" |States|");
+    System.out.println(" +------+");
+  }
+
+  public static void base3(){
+    System.out.println(" |      |");
+    System.out.println(" |      |");
+    System.out.println(" +------+");
+  }
+
+  public static void enging(){
+    System.out.println("    /\\ ");
+    System.out.println("   /  \\ ");
+    System.out.println("  /    \\ ");
+  }
+}
